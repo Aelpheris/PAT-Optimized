af586d833f50237260427c622bf94dcc02feabed
aelpheris <aelpheris@proton.me>
2025-04-08 16:22:54 -0400

    Set up live http server to manage cors and nodemon to watch file changes during development

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+const image = new Image();
+image.crossOrigin = "Anonymous";
+image.src = './map.png';
+
 const canvas = document.getElementById('map');
 const ctx = canvas.getContext('2d');
+const cellCanvas = document.getElementById('selected-cell');
+const cellCtx = cellCanvas.getContext('2d');
 
-const image = new Image();
+let cells = [];
+
 image.onload = draw;
 
-image.src = 'map.png';
 
 function draw() {
-  canvas.width = this.naturalWidth;
-  canvas.height = this.naturalHeight;
+  canvas.width = image.naturalWidth;
+  canvas.height = image.naturalHeight;
 
   console.log('width: ', canvas.width);
   console.log('height: ', canvas.height);
 
-  ctx.drawImage(this, 0, 0);
+  ctx.drawImage(image, 0, 0);
+  cellCtx.fillRect(0, 0, 56, 56);
 
-  drawGrid();
+  gridToCells(canvas, ctx);
+
+  // drawGrid();
 }
 
 function drawGrid() {
@@ -53,7 +62,7 @@ function drawGrid() {
 
 function clipCell(image, x, y, width, height) {
   
-  var canvas = document.createElement('canvas'),
+  let canvas = document.createElement('canvas');
   ctx = canvas.getContext('2d');
 
   canvas.width = width;
@@ -64,3 +73,74 @@ function clipCell(image, x, y, width, height) {
 
   return canvas;
 }
+
+function selectCell(e, dest, canvas, ctx) {
+  const bounding = canvas.getBoundingClientRect();
+  const x = e.clientX - bounding.left;
+  const y = e.clientY - bounding.top;
+  const pixel = ctx.getImageData(x, y, 1, 1);
+  const data = pixel.data;
+
+  const rgbColor = `rgb(${data[0]} ${data[1]} ${data[2]} / ${data[3] / 255})`;
+  dest.style.background = rgbColor;
+  dest.textContent = rgbColor;
+
+  console.log('pixel width: ', x);
+  console.log('pixel height: ', y);
+  console.log('pixel color: ', rgbColor);
+
+  return rgbColor;
+}
+
+function firstCell(e, dest, canvas, ctx) {
+  const bounding = canvas.getBoundingClientRect();
+  const x = e.clientX - bounding.left;
+  const y = e.clientY - bounding.top;
+  const pixel = ctx.getImageData(x, y, 14, 14);
+  const data = pixel.data;
+
+  console.log('pixel width: ', x);
+  console.log('pixel height: ', y);
+}
+
+// Splits the entire grid into a collection of cells to be searchable and usable for
+// selecting individual cells
+function gridToCells(canvas, ctx) {
+  let cell = document.createElement('canvas');
+  ctx = cell.getContext('2d');
+
+  cell.width = 14;
+  cell.height = 14;
+
+
+  for (let y = 0; y <= canvas.height; y += cell.height) {
+    for (let x = 0; x <= canvas.width; x += cell.width) {
+      const cell = {
+        'sx': x,
+        'sy': y,
+        // 'data': ctx.getImageData(x, y, 14, 14)
+      }
+      cells.push(cell);
+    }
+  }
+
+  console.log('number of cells: ', cells.length);
+  console.log('first cell: ', cells[0]);
+  return cells;
+}
+
+function showCell(id) {
+
+  console.log('showCell');
+
+  console.log('cell: ', cells[id]);
+
+  // cellCtx.putImageData(cells[0].data, 0, 0);
+
+  cellCtx.drawImage(canvas, cells[id].x, cells[id].y, 14, 14, 0, 0, 56, 56);
+}
+
+canvas.addEventListener('click', (event) => {
+  console.log('clicked');
+  selectCell(event, cellCanvas, canvas, ctx);
+});
